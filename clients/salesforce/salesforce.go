0b70da42a3fbd20337e4ba147e9edd94058fdcb6
thatmattlove <matt@stunninglyclear.com>
2024-07-01 15:05:58 -0400

    small refactor; update message text

--- clients/salesforce/salesforce.go
+++ clients/salesforce/salesforce.go
@@ -15,39 +15,42 @@ import (
 	twilioApi "github.com/twilio/twilio-go/rest/api/v2010"
 )
 
-func AuthenticateRequest(req *http.Request) (err error) {
+func AuthenticateRequest(req *http.Request) error {
 	env, err := util.LoadEnv()
 	if err != nil {
-		return
+		return err
 	}
 	key := req.Header.Get("x-api-key")
-	err = util.ValidateKey(key, env.AuthChecksumSalesforce)
-	return
+	return util.ValidateKey(key, env.AuthChecksumSalesforce)
 }
 
-func ValidateSalesforceData(codeRequest *types.SalesforceCodeRequest) (data *types.SalesforceCodeRequest, err error) {
-	err = validator.New().Struct(codeRequest)
+func ValidateSalesforceData(codeRequest *types.SalesforceCodeRequest) (*types.SalesforceCodeRequest, error) {
+	var data *types.SalesforceCodeRequest
+	err := validator.New().Struct(codeRequest)
 	if err != nil {
-		return
+		return nil, err
 	}
 	country := countries.ByName(codeRequest.Country)
 	if country == countries.Unknown {
 		err = fmt.Errorf("country '%s' is invalid or unknown", codeRequest.Country)
-		return
+		return nil, err
 	}
 	p, err := phonenumbers.Parse(codeRequest.PhoneNumber, country.Alpha2())
+	if err != nil {
+		return nil, err
+	}
 	phoneNumber := phonenumbers.Format(p, phonenumbers.E164)
 	data = codeRequest
 	data.PhoneNumber = phoneNumber
-	return
+	return data, nil
 }
 
-func HandleSalesforceCodeRequest(codeRequest *types.SalesforceCodeRequest) (err error) {
+func HandleSalesforceCodeRequest(codeRequest *types.SalesforceCodeRequest) error {
 	env, err := util.LoadEnv()
 	if err != nil {
-		return
+		return err
 	}
-	body := fmt.Sprintf("Your Support PIN for Case %s is %s. Reply STOP to opt out.", codeRequest.CaseNumber, codeRequest.Code)
+	body := fmt.Sprintf("Your Stellar Support PIN is %s\nReply STOP to opt out.", codeRequest.Code)
 	twilioClient := twilio.NewRestClientWithParams(twilio.ClientParams{
 		Username: env.TwilioAccountSID,
 		Password: env.TwilioAuthToken,
@@ -58,13 +61,13 @@ func HandleSalesforceCodeRequest(codeRequest *types.SalesforceCodeRequest) (err 
 	params.SetBody(body)
 	res, err := twilioClient.Api.CreateMessage(params)
 	if err != nil {
-		return
+		return err
 	}
 	resBody, err := json.Marshal(*res)
 	if err != nil {
 		log.Fatal(err)
-		return
+		return err
 	}
 	log.Writer().Write(resBody)
-	return
+	return nil
 }
